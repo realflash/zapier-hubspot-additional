59f2eb1cca101cd85c9a97a2390be517c857d1e5
Ian Gibbs <igibbs@cpan.org>
2021-11-18 19:37:19 +0000

    Add test for state inclusion

--- test/searches/find_tickets.js
+++ test/searches/find_tickets.js
@@ -5,20 +5,18 @@ const zapier = require('zapier-platform-core');
 const App = require('../../index');
 const appTester = zapier.createAppTester(App);
 
-describe('Search - find_tickets', () => {
+describe('Search - find_tickets no state filter', () => {
   zapier.tools.env.inject();
 
   it('should get an array', async () => {
     const bundle = {
       authData: {
         access_token: process.env.ACCESS_TOKEN,
-        oauth_consumer_key: process.env.OAUTH_CONSUMER_KEY,
-        oauth_consumer_secret: process.env.OAUTH_CONSUMER_SECRET,
-        oauth_token: process.env.OAUTH_TOKEN,
-        oauth_token_secret: process.env.OAUTH_TOKEN_SECRET,
       },
 
-      inputData: {},
+      inputData: {
+			filter: '[{"filters":[{"value":"G8NYJ","propertyName":"vehicle","operator":"EQ"}]}]'
+		},
     };
 
     const results = await appTester(
@@ -29,4 +27,34 @@ describe('Search - find_tickets', () => {
     results.length.should.be.aboveOrEqual(1);
     results[0].should.have.property('id');
   });
+  
+  
+});
+
+describe('Search - find_tickets with state filter', () => {
+  zapier.tools.env.inject();
+
+  it('should get an array', async () => {
+    const bundle = {
+      authData: {
+        access_token: process.env.ACCESS_TOKEN,
+      },
+
+      inputData: {
+			filter: '[{"filters":[{"value":"G8NYJ","propertyName":"vehicle","operator":"EQ"}]}]',
+			state_inclusion: {
+				4: 1
+			}
+		},
+    };
+
+    const results = await appTester(
+      App.searches['find_tickets'].operation.perform,
+      bundle
+    );
+    results.should.be.an.Array();
+    results.length.should.equal(1);
+  });
+  
+  
 });
